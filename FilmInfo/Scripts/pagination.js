5155035335c41f49e70c607d9b7f26acca152f76
ssezmor <ssezmor@gmail.com>
2015-06-22 15:54:36 +0300

    22.06

--- FilmInfo/Scripts/pagination.js
+++
@@ -1,9 +0,0 @@
-﻿$('.demo1').bootpag({
-    total: 5
-}).on("page", function (event, num) {
-    $(".content").html("Page " + num); // or some ajax content loading...
-
-    // ... after content load -> change total to 10
-    $(this).bootpag({ total: 10, maxVisible: 10 });
-
-});
