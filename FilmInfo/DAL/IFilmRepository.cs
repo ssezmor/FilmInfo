c2e0c2f6b9a7e6f15d3f912d5520ae5bf1da1baf
ssezmor <ssezmor@gmail.com>
2015-06-26 16:34:12 +0300

    2606

---
+++ FilmInfo/DAL/IFilmRepository.cs
@@ -0,0 +1,18 @@
+﻿using FilmInfo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FilmInfo.DAL
+{
+    public class IFilmRepository : IDisposable
+    {
+        IEnumerable<Film> GetFilms();
+        Film GetFilmByID(int filmId);
+        void InsertFilm(Film film);
+        void DeleteFilm(int filmID);
+        void UpdateFilm(Film film);
+        void Save();
+    }
+}
